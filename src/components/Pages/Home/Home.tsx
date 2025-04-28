450ada7c3761d882ae310e410d8a58e29cbc4e61
zouziszzm <uaepeekawariour@gmail.com>
2025-04-28 23:21:17 +0530

    chore: layout change

--- src/components/Pages/Home/Home.tsx
+++ src/components/Pages/Home/Home.tsx
@@ -6,25 +6,28 @@ import AnimatedContainer from '../../Uniqcomps/Animations/ContentAnimations/Anim
 const Home = () => {
   return (
     <div className="w-full flex justify-center">
-      <div className="max-w-8xl w-full">
-        <div className="flex-col flex w-full justify-center items-center 4xl:flex-row  mt-14">
-          <div className="w-[150px] h-[150px] 4xl:w-[25%] 4xl:h-[700px] relative">
-            <Image
-              src="/profile.svg"
-              fill
-              alt="Picture of the author"
-              className="rounded-full border-1 border-white w-fit h-fit 4xl:rounded-none 4xl:border-0"
-            />
-          </div>
-          <div className="w-fit 4xl:w-2/3 p-2">
+      <div className="max-w-8xl w-full justify-center items-center flex flex-col pt-20 4xlpt-40 ">
+        <div className="w-[150px] h-[150px] relative">
+          <Image
+            src="/profile.png"
+            width={150}
+            height={150}
+            alt="Picture of the author"
+            className="rounded-full border-1 border-borders w-fit h-fit"
+          />
+        </div>
+        <div className="w-fit 4xl:w-2/3 ">
+          <div className="p-4">
             <AnimatedContainer>
-              <LangText
-                en="About me"
-                jp="自己紹介"
-                styleJp="font-Jp"
-                styleEn="font-En"
-                classname="font-medium text-4xl text-center 4xl:text-left"
-              />
+              <div className="py-4">
+                <LangText
+                  en="About me"
+                  jp="自己紹介"
+                  styleJp="font-Jp"
+                  styleEn="font-En"
+                  classname=" text-4xl"
+                />
+              </div>
             </AnimatedContainer>
             <AnimatedContainer>
               <div className="flex flex-col gap-4">
@@ -33,7 +36,7 @@ const Home = () => {
                   jp="私はインド・カルナータカ州出身のデジタルクリエイターで、フロントエンド開発を中心に、バックエンド開発のフリーランスとしても活動しています。創造性を活かして、直感的なデザインを構築し、企業のデジタル成長をサポートしています。"
                   styleJp=""
                   styleEn="text-justify"
-                  classname="font-Cm 3xl:text-lg font-light "
+                  classname="font-Cm 3xl:text-lg "
                 />
 
                 <LangText
@@ -41,7 +44,43 @@ const Home = () => {
                   jp="フロントエンドの洗練された技術とバックエンドの設計を融合させ、シームレスなユーザー体験を追求しています。コードの世界を離れるときは、ゲームでリフレッシュしながら、創造力を保っています。"
                   styleJp=""
                   styleEn="text-justify"
-                  classname="font-Cm 3xl:text-lg font-light"
+                  classname="font-Cm 3xl:text-lg "
+                />
+              </div>
+            </AnimatedContainer>
+          </div>
+          <div className="p-4 flex flex-col gap-4 3xl:flex-row">
+            <AnimatedContainer>
+              <div className="py-4">
+                <LangText
+                  en="Eucation"
+                  jp="学歴"
+                  styleJp="font-Jp"
+                  styleEn="font-En"
+                  classname=" text-4xl"
+                />
+              </div>
+              <div className="flex flex-col 3xl:flex-row gap-4 3xl:gap-20">
+                <div className="font-Cm">
+                  <p>2022</p>
+                  <p>Masters&apos;s In Computer Applications</p>
+                  <p>P.E.S</p>
+                </div>
+                <div className="font-Cm">
+                  <p>2020</p>
+                  <p>Bachelor&apos;s In Computer Applications</p>
+                  <p>P.E.S</p>
+                </div>
+              </div>
+            </AnimatedContainer>
+            <AnimatedContainer>
+              <div className="py-4">
+                <LangText
+                  en="Eucation"
+                  jp="学歴"
+                  styleJp="font-Jp"
+                  styleEn="font-En"
+                  classname=" text-4xl"
                 />
               </div>
             </AnimatedContainer>
