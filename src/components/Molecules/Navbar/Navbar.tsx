22041543c4f3e84efe8590180d8c4cb5c2b95504
Farhaan.mantiqh <farhaan.mantiqh@gmail.com>
2025-05-10 23:48:18 +0530

    feat: reset and changes

--- src/components/Molecules/Navbar/Navbar.tsx
+++ src/components/Molecules/Navbar/Navbar.tsx
@@ -10,6 +10,8 @@ const Navbar = () => {
     { title: 'Projects', url: '/Projects' },
     { title: 'Theme', url: '#' }, // We'll handle the theme toggle separately
     { title: 'Lang', url: '#' }, // We'll handle the language toggle separately
+    { title: 'Langs', url: '#' }, // We'll handle the language toggle separately
+
   ];
 
   const { toggleTheme } = useTheme();
