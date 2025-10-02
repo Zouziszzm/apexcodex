2cfc8d2e88aaaa965fac823bd24272c7b0cb0c85
Al Farhaan khan I Inamdar <63239385+Zouziszzm@users.noreply.github.com>
2025-10-02 23:12:30 +0530

    Update Home.tsx

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 const Home = () => {
-  return <div className=" py-[25vh]">A</div>;
+  return <div className=" py-[25vh]">Ahona </div>;
 };
 
 export default Home;
