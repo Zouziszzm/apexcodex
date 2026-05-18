908f727a5d223d303971220e07fc936217ae1874
Al Farhaan khan I Inamdar <63239385+Zouziszzm@users.noreply.github.com>
2026-05-18 21:12:40 +0530

    Update socials.ts

--- data/socials.ts
+++ data/socials.ts
@@ -6,9 +6,5 @@ export interface SocialLink {
 export const socialLinks: SocialLink[] = [
   { label: "Email", href: "mailto:farhumaid@gmail.com" },
   { label: "X", href: "https://x.com/zouziszzm/" },
-  {
-    label: "LinkedIn",
-    href: "https://www.linkedin.com/in/al-farhaan-khan-inamdar/",
-  },
   { label: "GitHub", href: "https://github.com/zouziszzm" },
 ];
